d6c1de4920777866e05207738fc376909ece62c8
Andrea <andrealorenzosacchi@gmail.com>
2020-12-10 17:52:54 +0100

    Improved RPM and Throttle precision, added new command

--- ECUSerial/main.py
+++ ECUSerial/main.py
@@ -2,7 +2,7 @@ import serial, time
 from struct import unpack, pack
 ser = serial.Serial("COM5", 9600, timeout=5)
 matrix_length = 860
-
+throttle_indexs = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 data = " "
 while(data is not ""):
     data = input(">")
@@ -17,19 +17,34 @@ while(data is not ""):
         print(ser.write(b'r'))
         eeprom_buf = ser.read(matrix_length)
         counter = 0
+        rpm = 0
+        print("      Throttle", end="\nRPM")
+        fmt = "{:<9}" * (len(throttle_indexs) + 1)
+        print(fmt.format("", *throttle_indexs))
         for i in range(0,430*2,20):
-                print(eeprom_buf[i:i+20].hex())
-                counter += 1
+            print("{:05} {}".format(rpm, [f'{unpack("<h", eeprom_buf[i:i+20][j:j+2])[0]:05}' for j in range(0, 20, 2)]))
+            counter += 1
+            rpm += 250
         print(counter)
     elif(data == 'm'):
         print("Trying to read injectorMAP")
         print(ser.write(b'm'))
         injector_map = ser.read(matrix_length)
         counter = 0
+        rpm = 0
+        print("      Throttle", end="\nRPM")
+        fmt = "{:<9}" * (len(throttle_indexs) + 1)
+        print(fmt.format("", *throttle_indexs))
         for i in range(0,430*2,20):
-                print(injector_map[i:i+20].hex())
-                counter += 1
+            print("{:05} {}".format(rpm, [f'{unpack("<h", injector_map[i:i+20][j:j+2])[0]:05}' for j in range(0, 20, 2)]))
+            counter += 1
+            rpm += 250
         print(counter)
+    elif(data == 'i'):
+        print("Reading realtime data")
+        print(ser.write(b'i'))
+        info = ser.read(4*2 + 3)
+        print(info)
 
 print("Closing Serial comunication!")
 
